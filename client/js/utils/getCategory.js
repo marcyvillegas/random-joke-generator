e2f6e70e23baca9a2f1ad8bf922b15a7144ccff4
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2022-04-21 21:44:26 +0800

    fixing the comments

--- client/js/utils/getCategory.js
+++ client/js/utils/getCategory.js
@@ -1,19 +1,24 @@
 
-/* LOGIC */
-// Once button is clicked, it will change the value of the variable that will be exported
-// based on the endpoint name for every category
+/**
+ * LOGIC
+ * 1. Once button is clicked, it will change the value of the variable that will be exported based on the endpoint name for every category
+ * 2. It will proceed to the secondPage.html
+ */
 
-/* Declare variables */
+/* Declare variables of joke category buttons */
 let programmingButton = document.querySelector("#programming-button");
 let spoolyButton = document.querySelector("#spooky-button");
 let christmasButton = document.querySelector("#christmas-button");
 let punButton = document.querySelector("#pun-button");
 
+/**
+ * 
+ */
 programmingButton.addEventListener("click", (e) => {
 
     e.preventDefault();
 
+    console.log('hi');
+
     window.location.href="secondPage.html";
-
 });
-
